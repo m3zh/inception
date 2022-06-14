759606e8dbe6c81f5ac1985d0ae43155fd749b70
m3zh <27877964+m3zh@users.noreply.github.com>
2022-06-14 17:07:07 +0200

    wp tools config

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -12,12 +12,17 @@
 
 rm -rf /etc/nginx/sites-available/default
 rm -rf /etc/nginx/sites-enabled/default
-mkdir /var/www/myserver/phpmyadmin
-mkdir /var/www/myserver/wordpress
-wget https://files.phpmyadmin.net/phpMyAdmin/5.0.3/phpMyAdmin-5.0.3-english.tar.gz
-tar -xvf phpMyAdmin-5.0.3-english.tar.gz --strip-components=1 -C /var/www/myserver/phpmyadmin
-rm -rf phpMyAdmin-5.0.3-english.tar.gz
-rm /var/www/myserver/phpmyadmin/config.sample.inc.php
+sudo mkdir -p /var/www # keep or remove
+sudo chown www-data: /var/www
+mkdir /var/www/wordpress
+curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /srv/www
+sudo mysql -u root
+
 rm -rf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 wget https://wordpress.org/latest.tar.gz 
 tar -xvf latest.tar.gz --strip-components=1 -C /var/www/myserver/wordpress && rm -rf latest.tar.gz
+
+sudo -u www-data cp /srv/www/wordpress/wp-config-sample.php /srv/www/wordpress/wp-config.php
+sudo -u www-data sed -i 's/database_name_here/wordpress/' /srv/www/wordpress/wp-config.php
+sudo -u www-data sed -i 's/username_here/wordpress/' /srv/www/wordpress/wp-config.php
+sudo -u www-data sed -i 's/password_here/<your-password>/' /srv/www/wordpress/wp-config.php
